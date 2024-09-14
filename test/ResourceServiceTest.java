557ef0abf4f4ef642cb87b36c62a689cab58cfe7
timoheyn22 <166369125+timoheyn22@users.noreply.github.com>
2024-09-14 15:41:13 +0200

    Resource Test Update

--- test/ResourceServiceTest.java
+++ test/ResourceServiceTest.java
@@ -1,38 +1,56 @@
-/*package test;
+package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import src.Resource.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import src.Booking.Booking;
-import src.Booking.GermanBookingBuilder;
-import src.Resource.Car;
-import src.Resource.Resource;
-import src.Resource.ResourceService;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class ResourceServiceTest {
+class ResourceServiceTest {
+    private ResourceService resourceService;
+
+    @BeforeEach
+    void setUp() {
+        resourceService = new ResourceService();
+    }
 
     @Test
-    public void testGetSelectedResource() {
-        // Buchung erstellen
-        Booking booking = new GermanBookingBuilder().buildGermanBooking();
+    void testAddResource() {
+        Resource car = new Car("TestCar");
+        resourceService.addResource(car);
 
-        // ResourceService verwenden
-        ResourceService resourceService = new ResourceService();
-
-        // Test der Car Resource
-        Car carResource = (Car) resourceService.getSelectedResource("Car", booking);
-        assertNotNull(carResource);
-        assertTrue(carResource.getDetails().contains("Car Resource"));
-
-        // Test der SetTopBox Resource
-        Resource setTopBoxResource = resourceService.getSelectedResource("SetTopBox", booking);
-        assertNotNull(setTopBoxResource);
-        assertTrue(setTopBoxResource.getDetails().contains("SetTopBox Resource"));
-
-        // Test der ChildSeat Resource
-        Resource childSeatResource = resourceService.getSelectedResource("ChildSeat", booking);
-        assertNotNull(childSeatResource);
-        assertTrue(childSeatResource.getDetails().contains("ChildSeat Resource"));
+        Resource retrievedResource = resourceService.getSelectedResource("TestCar");
+        assertNotNull(retrievedResource, "Resource should be added and retrieved successfully.");
+        assertEquals("TestCar", retrievedResource.getName(), "Resource name should match.");
     }
+
+    @Test
+    void testRemoveResource() {
+        Resource car = new Car("TestCar");
+        resourceService.addResource(car);
+
+        resourceService.removeResource("TestCar");
+
+        Resource retrievedResource = resourceService.getSelectedResource("TestCar");
+        assertNull(retrievedResource, "Resource should be removed successfully.");
+    }
+
+    @Test
+    void testGetSelectedResource() {
+        Resource car = new Car("TestCar");
+        Resource setTopBox = new SetTopBox("TestSetTopBox");
+        resourceService.addResource(car);
+        resourceService.addResource(setTopBox);
+
+        Resource retrievedCar = resourceService.getSelectedResource("TestCar");
+        Resource retrievedSetTopBox = resourceService.getSelectedResource("TestSetTopBox");
+
+        assertNotNull(retrievedCar, "Car resource should be retrieved successfully.");
+        assertEquals("TestCar", retrievedCar.getName(), "Retrieved car name should match.");
+
+        assertNotNull(retrievedSetTopBox, "SetTopBox resource should be retrieved successfully.");
+        assertEquals("TestSetTopBox", retrievedSetTopBox.getName(), "Retrieved SetTopBox name should match.");
+    }
+
 }
-*/
